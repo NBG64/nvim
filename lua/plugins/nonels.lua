d74dd4e2b50647c222723905125c48396295502f
Noah <nbaylis64@gmail.com>
2024-08-21 22:05:08 -0400

    added tmux stuff

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -4,7 +4,7 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.styllua,
+                --null_ls.builtins.formatting.styllua,
                 --null_ls.builtins.formatting.black,
                 --null_ls.builtins.diagnostics.flake8,
                 --null_ls.builtins.formatting.prettier
--- lua/vimoptions.lua
+++ lua/vimoptions.lua
@@ -7,3 +7,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.g.mapleader= " "
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
