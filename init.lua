5f963aea61214909fa593c2ac0761a4f86da038b
NBG64 <nbaylis64@gmail.com>
2025-01-02 23:34:28 -0400

    New nvim config to start 2025

--- init.lua
+++ init.lua
@@ -1,20 +1,54 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+vim.g.mapleader= " "
 
-require("vimoptions")
-require("lazy").setup("plugins")
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+
+-- Use 'mini.deps'. `now()` and `later()` are helpers for a safe two-stage
+-- startup and are optional.
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+-- Safely execute immediately
+now(function()
+  vim.o.termguicolors = true
+  vim.cmd('colorscheme minicyan')
+end)
+now(function()
+  require('mini.notify').setup()
+  vim.notify = require('mini.notify').make_notify()
+end)
+now(function() require('mini.icons').setup({style = 'ascii'}) end)
+now(function() require('mini.tabline').setup() end)
+now(function() require('mini.statusline').setup() end)
+now(function() require('mini.bracketed').setup() end)
+now(function() require('mini.comment').setup() end)
+now(function() require('mini.pick').setup() end)
+now(function() require('mini.files').setup() end)
+now(function() require('mini.indentscope').setup() end)
+now(function() require('mini.move').setup() end)
+now(function() require('mini.pairs').setup() end)
+now(function() require('mini.visits').setup() end)
+-- Safely execute later
+--later(function() require('mini.surround').setup() end)
 
